20e9cd64a4d189f24ee874bc776ad9197fa42426
arashad202 <ahm.rashad.202@gmail.com>
2025-07-19 05:29:47 +0300

    Fetching Cards
    use useEffect to fetch cards when component is
    mounted only (empty dependencies array) and
    shuffle them to setCards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import Scoreboard from "./components/Scoreboard";
 import CardGrid from "./components/CardGrid";
-// import { useState } from "react"
+import { useEffect, useState } from "react"
+
+const API_KEY = "NeMdd73RpQDTzlN6ehutF17btqtYrK4n"
 
 function App() {
-  let currentScore;
-  let bestScore;
-  let cards = [];
-  let handleCardClick;
+  const [cards, setCards] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    const fetchGifs = async () => {
+      try {
+
+        const res = await fetch(
+          `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=12`
+        )
+        const data = await res.json();
+
+        const formatted = data.data.map((gif) => ({
+          id: gif.id,
+          url: gif.images.fixed_height.url,
+          title: gif.title,
+        }));
+
+        setCards(shuffleCards(formatted));
+
+      } catch (err)  {
+        console.log(err);
+      }
+
+      fetchGifs();
+    }
+  }, []);
+
+  const shuffleCards = (cardsArray) => {
+    return [...cardsArray].sort(() => Math.round() - 0.5);
+  }
+
+
 
 
   return (
